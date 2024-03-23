1cbc6e6d2af99d613f74ad1a4cf51f4c57075098
Gibran Khan <g.khan@posteo.net>
2024-03-23 17:46:00 +0200

    * updated to use separate datasets * now on NC28 * performance improvements

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # NextCloud on FreeBSD
 
-Script to automate installation of Nextcloud on FreeBSD13+ and HardenedBSD13.
-The finished installation passes all nextcloud configuration checks.
+Script to automate installation of Nextcloud on FreeBSD13+ and HardenedBSD13+
+The finished installation passes all Nextcloud configuration checks.
 This script follows recommended configuration as per https://docs.nextcloud.com/server/stable/admin_manual/installation/system_requirements.html
 
 ## Instructions
 
+0. Read the instructions, and the scripts! :)
 1. Clone repository or download release to your machine and extract.
 2. `cd` to folder.
 3. Switch to root by using `su`.
@@ -16,15 +17,15 @@ This script follows recommended configuration as per https://docs.nextcloud.com/
 7. Change the values of variables as required to suite your environment.
 8. Save the file.
 9. Run `install.sh`
-10. Please be patient while the script runs
-11. Done
+10. Please be patient while the script runs and drink your prefferred beverage.
+11. Enjoy
 
 **Installs the following:**
 
-* Nextcloud 27
+* Nextcloud 28
 * Apache 2.4
 * MariaDB 10.6
-* PHP 8.2 (plus all php-extensions required)
+* PHP 8.2 (plus all required php-extensions)
 * Redis
 * ClamAV
 
@@ -47,7 +48,7 @@ This script follows recommended configuration as per https://docs.nextcloud.com/
 * Deck
 * Mail
 * Notes
-* Nextcloud Talk
+* Nextcloud Talk (Spreed)
 * Tasks
 * External storage support (including `samba` and `ftp`) (Can be disabled independently)
   
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #
 # Install Nextcloud on FreeBSD/HardenedBSD
 #
-# Last update: 2023-08-29
+# Last update: 2024-03-22
 # https://github.com/theGeeBee/NextCloudOnFreeBSD/
 #
 
@@ -16,6 +16,9 @@ if [ "$(id -u)" -ne 0 ]; then
    exit 1
 fi
 
+# Check if HBSD is present in uname string
+hbsd_test=$(uname -a | grep -o 'HBSD')
+
 # Load config settings
 CONFIG_FILE="${PWD}/install.conf"
 
@@ -27,12 +30,14 @@ else
 fi
 
 #
-# Set `pkg` to use LATEST (optional)
+# Set `pkg` to use LATEST if set (default setting)
 #
-mkdir -p /usr/local/etc/pkg/repos
-echo "FreeBSD: { enabled: no }" > /usr/local/etc/pkg/repos/FreeBSD.conf
-cp /etc/pkg/FreeBSD.conf /usr/local/etc/pkg/repos/nextcloud.conf
-sed -i '' "s|quarterly|latest|" /usr/local/etc/pkg/repos/nextcloud.conf
+if [ "$FREEBSD_REPO" = "latest" ]; then
+	mkdir -p /usr/local/etc/pkg/repos
+	echo "FreeBSD: { enabled: no }" > /usr/local/etc/pkg/repos/FreeBSD.conf
+	cp /etc/pkg/FreeBSD.conf /usr/local/etc/pkg/repos/nextcloud.conf
+	sed -i '' "s|quarterly|latest|" /usr/local/etc/pkg/repos/nextcloud.conf
+fi
 
 #
 # Install `pkg`, update repository, upgrade existing packages
@@ -44,6 +49,39 @@ pkg upgrade -y
 
 # Install required packages
 xargs pkg install -y < "${PWD}/includes/requirements.txt"
+
+# Check if not running in a jail + CREATE_DATASETS=true
+# then create datasets for mariaDB's databases and log files that are optimised
+# Also create dataset for nextcloud data - this is not useful if you have multiple datasets
+# because it creates it under the root dataset, so modify accordingly, or turn off and create as a directory
+# use recommended settings from this script as a guide.
+if [ "$(sysctl -n security.jail.jailed)" -ne 1 ] && [ "$CREATE_DATASETS" = "true" ]; then
+	# Remove the directories created by MariaDB so that we can create new ZFS datasets in their place.
+	rm -r /var/db/mysql
+	rm -r /var/log/mysql
+	# Find the ZFS dataset containing the root file system
+	zfs_dataset=$(zfs list -H -o name -r -t filesystem -o name,mountpoint | awk 'NR==1 {print $1}')
+
+	# Check if the dataset is found
+	if [ -z "$zfs_dataset" ]; then
+		echo "Error: Root ZFS dataset not found."
+		exit 1
+	fi
+	# Create the MariaDB dataset with the desired properties, including primarycache=metadata
+	zfs create -o recordsize=16K -o aclmode=restricted -o mountpoint=/var/db/mysql -o primarycache=metadata -o compression=lz4 "$zfs_dataset"/mariadb_data
+	# Create the second dataset for /var/log/mysql with the same properties
+	zfs create -o recordsize=128K -o aclmode=restricted -o mountpoint=/var/log/mysql -o primarycache=metadata -o compression=lz4 "$zfs_dataset"/mariadb_logs
+	# Set ownership to the MySQL user for both datasets
+	chown -R mysql:mysql /var/db/mysql /var/log/mysql
+	# Create Nextcloud dataset with the desired properties
+	zfs create -o recordsize=16K -o aclmode=restricted -o mountpoint="${DATA_DIRECTORY}" -o primarycache=metadata -o compression=lz4 "${zfs_dataset}/${DATASET}"
+	# Set ownership to the www user for the dataset
+	chown www:www "${DATA_DIRECTORY}"
+else
+	# Create the Nextcloud data directory and set ownership to the www user
+	mkdir -p "${DATA_DIRECTORY}"
+	chown www:www "${DATA_DIRECTORY}"
+fi
 
 # Download virus definitions
 freshclam
@@ -66,9 +104,6 @@ then
 	echo "The Nextcloud download is corrupt."
 	exit 1
 fi
-
-# Check if HBSD is present in uname string
-hbsd_test=$(uname -a | grep -o 'HBSD')
 
 # Set `sysctl` values (necessary for `redis`)
 sysctl kern.ipc.somaxconn=1024
@@ -103,7 +138,8 @@ pw usermod www -G redis
 
 # Extract Nextcloud and give `www` ownership of the directory
 tar xjf "/tmp/${FILE}" -C "${WWW_DIR}/"
-chown -R www:www "${WWW_DIR}/nextcloud"
+mv "${WWW_DIR}/nextcloud" "${WWW_DIR}/${HOST_NAME}"
+chown -R www:www "${WWW_DIR}/${HOST_NAME}"
 
 # Create self-signed SSL certificate
 if [ "$SSL_DIRECTORY" = "OFF" ]; then
@@ -118,8 +154,10 @@ fi
 #
 # Copy pre-writting config files and edit in place
 #
+sed -i '' "s|HOST_NAME|${HOST_NAME}|g" "${PWD}/includes/nextcloud.conf"
+sed -i '' "s|IP_ADDRESS|${IP_ADDRESS}|" "${PWD}/includes/nextcloud.conf"
 sed -i '' "s|IP_ADDRESS|${IP_ADDRESS}|" "${PWD}/includes/httpd.conf"
-sed -i '' "s|WWW_DIR|${WWW_DIR}|" "${PWD}/includes/httpd.conf"
+sed -i '' "s|EMAIL_ADDRESS|${EMAIL_ADDRESS}|" "${PWD}/includes/httpd.conf"
 sed -i '' "s|WWW_DIR|${WWW_DIR}|" "${PWD}/includes/nextcloud.conf"
 sed -i '' "s|SSL_DIRECTORY|${SSL_DIRECTORY}|" "${PWD}/includes/nextcloud.conf"
 sed -i '' "s|MYTIMEZONE|${TIME_ZONE}|" "${PWD}/includes/php.ini"
@@ -127,25 +165,30 @@ sed -i '' "s|MYTIMEZONE|${TIME_ZONE}|" "${PWD}/includes/php.ini"
 
 # Disable self-signed SSL certificate if SSL_DIRECTORY="OFF"
 if [ "$SSL_DIRECTORY" = "OFF" ]; then
+   sed -i '' "s|LISTEN_PORT|80|" "${PWD}/includes/nextcloud.conf"
    sed -i '' "s|LISTEN_PORT|80|" "${PWD}/includes/httpd.conf"
    sed -i '' "s|SSL_OFF_|# |" "${PWD}/includes/nextcloud.conf"
+   sed -i '' "s|SSL_OFF_|# |" "${PWD}/includes/httpd.conf"
 else
+   sed -i '' "s|LISTEN_PORT|443|" "${PWD}/includes/nextcloud.conf"
    sed -i '' "s|LISTEN_PORT|443|" "${PWD}/includes/httpd.conf"
    sed -i '' "s|SSL_OFF_||" "${PWD}/includes/nextcloud.conf"
+   sed -i '' "s|SSL_OFF_||" "${PWD}/includes/httpd.conf"
 fi
 
 # Disable PHP Just-in-Time compilation for HardenedBSD support
 if [ "$hbsd_test" ]
 	then
-		sed -i '' "s|pcre.jit=1|pcre.jit=0|" "${PWD}/inclues/php.ini"
-		sed -i '' "s|opcache.jit = 1255|opcache.jit = 0|" "${PWD}/inclues/php.ini"
-		sed -i '' "s|opcache.jit_buffer_size = 128M|opcache.jit_buffer_size = 0|" "${PWD}/inclues/php.ini"
+		sed -i '' "s|pcre.jit=1|pcre.jit=0|" "${PWD}/includes/php.ini"
+		sed -i '' "s|opcache.jit = 1255|opcache.jit = 0|" "${PWD}/includes/php.ini"
+		sed -i '' "s|opcache.jit_buffer_size = 128M|opcache.jit_buffer_size = 0|" "${PWD}/includes/php.ini"
 	fi
+mkdir /usr/local/etc/apache24/vhosts
 cp -f "${PWD}/includes/httpd.conf" /usr/local/etc/apache24/
 cp -f "${PWD}/includes/php.ini" /usr/local/etc/php.ini
 cp -f "${PWD}/includes/www.conf" /usr/local/etc/php-fpm.d/
 cp -f "${PWD}/includes/redis.conf" /usr/local/etc/redis.conf
-cp -f "${PWD}/includes/nextcloud.conf" /usr/local/etc/apache24/Includes/
+cp -f "${PWD}/includes/nextcloud.conf" "/usr/local/etc/apache24/vhosts/${HOST_NAME}.conf"
 cp -f "${PWD}/includes/030_php-fpm.conf" /usr/local/etc/apache24/modules.d/
 cp -f "${PWD}/includes/my.cnf" /usr/local/etc/mysql/
 
@@ -161,10 +204,6 @@ service mysql-server restart
 mkdir -p /var/log/nextcloud/
 chown www:www /var/log/nextcloud
 
-# Create NextCloud data directory
-mkdir -p "${DATA_DIRECTORY}"
-chown www:www "${DATA_DIRECTORY}"
-
 #
 # Create Nextcloud database, secure database, set MariaDB root password, create Nextcloud DB, user, and password
 #
@@ -173,8 +212,8 @@ mariadb -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('l
 mariadb -u root -e "DROP DATABASE IF EXISTS test;"
 mariadb -u root -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
 mariadb -u root -e "CREATE DATABASE ${DB_NAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
-mariadb -u root -e "CREATE USER '${DB_USERNAME}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USERNAME}'@'localhost';"
+mariadb -u root -e "CREATE USER '${DB_USERNAME}'@'127.0.0.1' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USERNAME}'@'127.0.0.1';"
 mariadb -u root -e "FLUSH PRIVILEGES;"
 mariadb-admin --user=root password "${DB_ROOT_PASSWORD}" reload
 
@@ -187,50 +226,50 @@ cp -f "${PWD}/includes/root_my.cnf" /root/.my.cnf
 #
 clear
 echo "Installing Nextcloud..."
-sudo -u www php "${WWW_DIR}/nextcloud/occ" maintenance:install --database="mysql" --database-name="${DB_NAME}" --database-user="${DB_USERNAME}" --database-pass="${DB_PASSWORD}" --database-host="127.0.0.1" --admin-user="${ADMIN_USERNAME}" --admin-pass="${ADMIN_PASSWORD}" --data-dir="${DATA_DIRECTORY}"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" db:add-missing-primary-keys
-sudo -u www php "${WWW_DIR}/nextcloud/occ" db:add-missing-indices
-sudo -u www php "${WWW_DIR}/nextcloud/occ" db:add-missing-columns
-sudo -u www php "${WWW_DIR}/nextcloud/occ" db:convert-filecache-bigint --no-interaction
-sudo -u www php "${WWW_DIR}/nextcloud/occ" maintenance:mimetype:update-db
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set default_phone_region --value="${COUNTRY_CODE}"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set logtimezone --value="${TIME_ZONE}"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set logdateformat --value="Y-m-d H:i:s T"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set log_type --value="file"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set logfile --value="/var/log/nextcloud/nextcloud.log"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set loglevel --value="2"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set logrotate_size --value="104847600"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set filelocking.enabled --value=true
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set memcache.local --value="\OC\Memcache\APCu"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set redis host --value="/var/run/redis/redis.sock"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set redis port --value=0 --type=integer
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set memcache.distributed --value="\OC\Memcache\Redis"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set memcache.locking --value="\OC\Memcache\Redis"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" maintenance:install --database="mysql" --database-name="${DB_NAME}" --database-user="${DB_USERNAME}" --database-pass="${DB_PASSWORD}" --database-host="127.0.0.1" --admin-user="${ADMIN_USERNAME}" --admin-pass="${ADMIN_PASSWORD}" --data-dir="${DATA_DIRECTORY}"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" db:add-missing-primary-keys
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" db:add-missing-indices
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" db:add-missing-columns
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" db:convert-filecache-bigint --no-interaction
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" maintenance:mimetype:update-db
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set default_phone_region --value="${COUNTRY_CODE}"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set logtimezone --value="${TIME_ZONE}"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set logdateformat --value="Y-m-d H:i:s T"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set log_type --value="file"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set logfile --value="/var/log/nextcloud/nextcloud.log"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set loglevel --value="2"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set logrotate_size --value="104847600"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set filelocking.enabled --value=true
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set memcache.local --value="\OC\Memcache\APCu"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set redis host --value="/var/run/redis/redis.sock"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set redis port --value=0 --type=integer
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set memcache.distributed --value="\OC\Memcache\Redis"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set memcache.locking --value="\OC\Memcache\Redis"
 if [ "$USE_HOSTNAME" = "true" ]; then
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set trusted_domains 0 --value="${HOST_NAME}"
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set overwritehost --value="${HOST_NAME}"
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set overwrite.cli.url --value="https://${HOST_NAME}"
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set trusted_domains 0 --value="${HOST_NAME}"
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set overwritehost --value="${HOST_NAME}"
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set overwrite.cli.url --value="https://${HOST_NAME}"
 else
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set trusted_domains 0 --value="${IP_ADDRESS}"
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set trusted_domains 1 --value="${HOST_NAME}"
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set overwrite.cli.url --value="https://${IP_ADDRESS}"
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set trusted_domains 0 --value="${IP_ADDRESS}"
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set trusted_domains 1 --value="${HOST_NAME}"
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set overwrite.cli.url --value="https://${IP_ADDRESS}"
 fi
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set overwriteprotocol --value="https"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set htaccess.RewriteBase --value="/"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" maintenance:update:htaccess
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set overwriteprotocol --value="https"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set htaccess.RewriteBase --value="/"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" maintenance:update:htaccess
 # Set Nextcloud to use sendmail (you can change this later in the GUI)
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set mail_smtpmode --value="sendmail"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set mail_sendmailmode --value="smtp"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set mail_domain --value="${HOST_NAME}"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:system:set mail_from_address --value="${EMAIL_ADDRESS}"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set mail_smtpmode --value="sendmail"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set mail_sendmailmode --value="smtp"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set mail_domain --value="${HOST_NAME}"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:system:set mail_from_address --value="${EMAIL_ADDRESS}"
 # Disable contactsinteraction because the behaviour is unwanted, and confusing
-sudo -u www php "${WWW_DIR}/nextcloud/occ" app:disable contactsinteraction
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:disable contactsinteraction
 # Enable external storage support (Example: mount a SMB share in Nextcloud).
 # Users are not allowed to mount external storage, but can be allowed under Settings -> Admin -> External Storage
 if [ "$EXTERNAL_STORAGE" = "true" ]; then
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" app:enable files_external
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" config:app:set files_external allow_user_mounting --value="no"
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" config:app:set files_external user_mounting_backends --value="ftp,dav,owncloud,sftp,amazons3,swift,smb,\\OC\\Files\\Storage\\SFTP_Key,\\OC\\Files\\Storage\\SMB_OC"
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:enable files_external
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:app:set files_external allow_user_mounting --value="no"
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:app:set files_external user_mounting_backends --value="ftp,dav,owncloud,sftp,amazons3,swift,smb,\\OC\\Files\\Storage\\SFTP_Key,\\OC\\Files\\Storage\\SMB_OC"
 fi
 
 #
@@ -239,13 +278,13 @@ fi
 if [ "$INSTALL_APPS" = "true" ]; then
 	clear
 	echo "Nextcloud is now installed, installing recommended Apps..."
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" app:install calendar
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" app:install contacts
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" app:install deck
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" app:install mail
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" app:install notes
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" app:install spreed # Nextcloud Talk
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" app:install tasks
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:install calendar
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:install contacts
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:install deck
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:install mail
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:install notes
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:install spreed # Nextcloud Talk
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:install tasks
 fi
 
 #
@@ -253,13 +292,13 @@ fi
 #
 clear
 echo "Now installing and configuring Antivirus for File using ClamAV..."
-sudo -u www php "${WWW_DIR}/nextcloud/occ" app:install files_antivirus
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:install files_antivirus
 ### set correct value for path on FreeBSD and set default action
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:app:set files_antivirus av_mode --value="socket"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:app:set files_antivirus av_socket --value="/var/run/clamav/clamd.sock"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:app:set files_antivirus av_stream_max_length --value="104857600"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:app:set files_antivirus av_infected_action --value="only_log"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" config:app:set activity notify_notification_virus_detected --value="1"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:app:set files_antivirus av_mode --value="socket"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:app:set files_antivirus av_socket --value="/var/run/clamav/clamd.sock"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:app:set files_antivirus av_stream_max_length --value="104857600"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:app:set files_antivirus av_infected_action --value="only_log"
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" config:app:set activity notify_notification_virus_detected --value="1"
 
 #
 # SERVER SIDE ENCRYPTION 
@@ -267,13 +306,13 @@ sudo -u www php "${WWW_DIR}/nextcloud/occ" config:app:set activity notify_notifi
 # This comes with limitations like a performance penalty, so enable this only if needed.
 #
 if [ "$ENCRYPT_DATA" = "true" ]; then
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" app:enable encryption
-	sudo -u www php "${WWW_DIR}/nextcloud/occ" encryption:enable
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" app:enable encryption
+	sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" encryption:enable
 fi
 
 # Set Nextcloud to run maintenace tasks as a cron job
 sed -i '' "s|WWW_DIR|${WWW_DIR}|" "${PWD}/includes/www-crontab"
-sudo -u www php "${WWW_DIR}/nextcloud/occ" background:cron
+sudo -u www php "${WWW_DIR}/${HOST_NAME}/occ" background:cron
 crontab -u www "${PWD}/includes/www-crontab"
 
 # Create reference file
@@ -309,4 +348,4 @@ cat "/root/${HOST_NAME}_reference.txt"
 echo "These details have also been written to /root/${HOST_NAME}_reference.txt"
 
 # Run the Nextcloud background task for the first time
-sudo -u www /usr/local/bin/php -f "${WWW_DIR}/nextcloud/cron.php" &
+sudo -u www /usr/local/bin/php -f "${WWW_DIR}/${HOST_NAME}/cron.php" &
